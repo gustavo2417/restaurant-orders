eca0ad16c84c8d4940bcffc06f6c6b90a1e07f60
Gustavo <gustavodarocha241@gmail.com>
2023-05-28 13:36:18 -0300

    feat: requisitos 1 e 2 completos

--- tests/dish/test_dish.py
+++ tests/dish/test_dish.py
@@ -1,6 +1,27 @@
 from src.models.dish import Dish  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient
+import pytest
 
 
 # Req 2
 def test_dish():
-    pass
+    dish = Dish("frango a milanesa", 50.00)
+
+    assert dish == Dish("frango a milanesa", 50.00)
+    assert repr(dish) == "Dish('frango a milanesa', R$50.00)"
+    assert dish.name == "frango a milanesa"
+    assert hash(dish) == hash(repr(dish))
+
+    ingredient1 = Ingredient("Peito de frango")
+    ingredient2 = Ingredient("Farinha de Rosca")
+    dish.add_ingredient_dependency(ingredient1, 1)
+    dish.add_ingredient_dependency(ingredient2, 1)
+
+    assert dish.get_restrictions() == set()
+    assert dish.get_ingredients() == set({ingredient1, ingredient2})
+    assert isinstance(dish, Dish)
+
+    with pytest.raises(TypeError):
+        Dish("Frango", "100.00")
+    with pytest.raises(ValueError):
+        Dish("Arroz", -10.00)
--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -3,4 +3,11 @@ from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
 
 # Req 1
 def test_ingredient():
-    pass
+    ingredient = Ingredient("hmmm comida")
+
+    assert ingredient.name == "hmmm comida"
+    assert ingredient == Ingredient("hmmm comida")
+    assert repr(ingredient) == "Ingredient('hmmm comida')"
+    assert hash(ingredient) == hash("hmmm comida")
+    assert isinstance(ingredient, Ingredient)
+    assert not ingredient.restrictions
